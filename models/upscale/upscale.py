b2ea87ff5177b5f6f40c7a8f87970a1854af3e8b
Ольга Чернышева <prof-chernish@rambler.ru>
2026-02-03 01:42:10 +0300

    Fix upscale.py

--- models/upscale/upscale.py
+++ models/upscale/upscale.py
@@ -2,9 +2,7 @@ from pathlib import Path
 
 import numpy as np
 import torch
-from basicsr.archs.srvgg_arch import SRVGGNetCompact
 from PIL import Image
-from realesrgan import RealESRGANer
 
 ROOT = Path(__file__).resolve().parents[2]  # корень проекта
 WEIGHTS_PATH = ROOT / "weights" / "realesr-general-x4v3.pth"
@@ -15,39 +13,53 @@ TILE_PAD = 10
 DEFAULT_SCALE = 2
 
 
-# инициализация модели
-_model = SRVGGNetCompact(
-    num_in_ch=3,
-    num_out_ch=3,
-    num_feat=64,
-    num_conv=32,
-    upscale=4,
-    act_type="prelu",
-)
+_upsampler = None
 
-_upsampler = RealESRGANer(
-    scale=4,
-    model_path=str(WEIGHTS_PATH),
-    model=_model,
-    tile=TILE,
-    tile_pad=TILE_PAD,
-    pre_pad=0,
-    half=False,
-    device=DEVICE,
-)
+
+def get_upsampler():
+    
+    global _upsampler
+
+    if _upsampler is not None:
+        return _upsampler
+
+    from basicsr.archs.srvgg_arch import SRVGGNetCompact
+    from realesrgan import RealESRGANer
+
+    model = SRVGGNetCompact(
+        num_in_ch=3,
+        num_out_ch=3,
+        num_feat=64,
+        num_conv=32,
+        upscale=4,
+        act_type="prelu",
+    )
+
+    _upsampler = RealESRGANer(
+        scale=4,
+        model_path=str(WEIGHTS_PATH),
+        model=model,
+        tile=TILE,
+        tile_pad=TILE_PAD,
+        pre_pad=0,
+        half=False,
+        device=DEVICE,
+    )
+
+    return _upsampler
+
 
 
 def upscale_image(pil_image: Image.Image, scale: int = DEFAULT_SCALE) -> Image.Image:
-    """
-    Апскейл изображения (×2 по умолчанию).
-    Вход/выход: PIL.Image
-    """
     if scale not in (2, 4):
         raise ValueError("scale должен быть 2 или 4")
 
     img = np.array(pil_image.convert("RGB"))
 
+    upsampler = get_upsampler()
+
     with torch.no_grad():
-        out, _ = _upsampler.enhance(img, outscale=scale)
+        out, _ = upsampler.enhance(img, outscale=scale)
 
     return Image.fromarray(out)
+
